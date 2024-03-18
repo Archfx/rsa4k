f3d836663dec44b6f0b9f17bde69b8c940740d34
archfx <cybx@live.com>
2024-03-17 22:58:07 -0400

    minor bug fixes

--- tb/top.v
+++ tb/top.v
@@ -61,23 +61,23 @@ module ModExp_tb();
 		cypher_prev = cypher;
 		go =0;
 
-        #10
-        reset = 1;
-        #10
-        reset = 0;
+        // #10
+        // reset = 1;
+        // #10
+        // reset = 0;
         #100
 
         go = 0;
 		#10
 
 
-        // message = 4096'd50;//cypher_prev;
-        // exponent =  4096'd37;
-        // modulus =  4096'd77;
-
-		message = 4096'd8;
-        exponent =  4096'd13;
+        message = cypher_prev;
+        exponent =  4096'd37;
         modulus =  4096'd77;
+
+		// message = 4096'd8;
+        // exponent =  4096'd13;
+        // modulus =  4096'd77;
 
         // message = 4096'h6a4a5a9d37dde9648c8e4f1ba3c73a8229755dc01355d9a2c6d61388c016073c0ebac73afc19dbc32d2a20b875ee44009ddfcc910005629453d314f13ac405a3ada039150bd12328286d4e09c4fe5e9e2484f63b033265af9df8ffb631014a2748b2f5d9a4c7b0c0da78d3b430b4b4c1892f32dc7b79ed4390f4a2bacd2bc45d4203b3c704b3ae54d6796053ac23674def1493bbd095d7e35dfdc39a1d169678e4a0d4c8e0df74807f8f48d8a28ec8e5209e16599c52d57883fd9a7e2123df4856029b11f5ae2b0818a7ca93b52ba7539586d092859be981bcca0740a38e81370d44f6c5f61bd6f0961bd4670c084341310b2bba0a0802e9e3ed5f6ce2aa65cc;
         // exponent= 4096'h19f9641ecc4e21405d238542fc633a35a33f3ddc84dfb8dc000c27bdfbb5128c0ae3b561ca615aabf7223824d6415a8a6285ca781683aa76ab9c8542dc02bc50ce60770246fa565a1975f6ce508d3cfc30b24b7eccc02183c5bf6a9b7900d621cb3e97fe57031574ab9e54b0eb2040415262cb7f354e032c39453ac38c51d9f9d98bccde0b866d5bbb4013b84054d55ecf8677a3af7308898ef03d30c796cb020aab69002ac00e820fd0bdb176c20589eee572f8699c27353dea73f7ea9b6c83a55d05f7eec7bb77244f11895ff0f462893ead61d8e51c55eea2ffc5b522c0b86fcde09c785f418570d071ddd39d8c3c3a05d80ef51a081ed3749180b973f24f;
